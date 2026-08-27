117692a395424ef4159549a7ddfce2e85ee5faef
duoduoyan1205 <emking.yan@gmail.com>
2026-08-27 18:44:50 +0800

    Fix Memory Pool metadata test for BufferDesc ABI

--- mooncake-transfer-engine/tests/transfer_metadata_test.cpp
+++ mooncake-transfer-engine/tests/transfer_metadata_test.cpp
@@ -1,153 +1,77 @@
-// Copyright 2024 KVCache.AI
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #include "transfer_metadata.h"
 
-#include <gflags/gflags.h>
-#include <glog/logging.h>
-#include <gtest/gtest.h>
-#include <sys/time.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #include <chrono>
-#include <cstdlib>
+#include <memory>
+#include <string>
 #include <thread>
 
-#include "config.h"
-#include "transport/transport.h"
-#include "transfer_metadata_plugin.h"
-
-using namespace mooncake;
+#include <gtest/gtest.h>
 
 namespace mooncake {
-
-class TransferMetadataTest : public ::testing::Test {
-   protected:
-    void SetUp() override {
-        google::InitGoogleLogging("TransferMetadataTest");
-        FLAGS_logtostderr = 1;
-
-        const char* env = std::getenv("MC_METADATA_SERVER");
-        if (env)
-            metadata_server = env;
-        else
-            metadata_server = metadata_server;
-        LOG(INFO) << "metadata_server: " << metadata_server;
-
-        env = std::getenv("MC_LOCAL_SERVER_NAME");
-        if (env)
-            local_server_name = env;
-        else
-            local_server_name = "127.0.0.2:12345";
-        LOG(INFO) << "local_server_name: " << local_server_name;
-
-        metadata_client = std::make_unique<TransferMetadata>(metadata_server);
-    }
-    void TearDown() override { google::ShutdownGoogleLogging(); }
-    std::unique_ptr<TransferMetadata> metadata_client;
-    std::string metadata_server;
-    std::string local_server_name;
-};
-
-TEST_F(TransferMetadataTest, LocalSegmentTest) {
-    auto segment_des = std::make_shared<TransferMetadata::SegmentDesc>();
-    segment_des->name = "test_server";
-    segment_des->protocol = "rdma";
-    TransferMetadata::SegmentID segment_id = 1111111;
-    std::string segment_name = "test_segment";
-    int re = metadata_client->addLocalSegment(segment_id, segment_name,
-                                              std::move(segment_des));
-    ASSERT_EQ(re, 0);
-    auto des = metadata_client->getSegmentDescByName(segment_name);
-    ASSERT_EQ(des, segment_des);
-    des = metadata_client->getSegmentDescByID(segment_id, false);
-    ASSERT_EQ(des, segment_des);
-    auto id = metadata_client->getSegmentID(segment_name);
-    ASSERT_EQ(id, segment_id);
-    re = metadata_client->removeLocalSegment(segment_name);
-    ASSERT_EQ(re, 0);
-}
-
-TEST_F(TransferMetadataTest, LocalMemoryBufferTest) {
-    auto segment_des = std::make_shared<TransferMetadata::SegmentDesc>();
-    segment_des->name = "test_localMemory";
-    segment_des->protocol = "rdma";
-    int re = metadata_client->addLocalSegment(
-        LOCAL_SEGMENT_ID, "test_local_segment", std::move(segment_des));
-    ASSERT_EQ(re, 0);
-    uint64_t addr = 0;
-    for (int i = 0; i < 10; ++i) {
-        TransferMetadata::BufferDesc buffer_des;
-        buffer_des.addr = addr + i * 2048;
-        buffer_des.length = 1024;
-        re = metadata_client->addLocalMemoryBuffer(buffer_des, false);
-        ASSERT_EQ(re, 0);
-    }
-    addr = 1000;
-    re = metadata_client->removeLocalMemoryBuffer((void*)addr, false);
-    ASSERT_EQ(re, ERR_ADDRESS_NOT_REGISTERED);
-    for (int i = 9; i > 0; --i) {
-        addr = i * 2048;
-        re = metadata_client->removeLocalMemoryBuffer((void*)addr, false);
-        ASSERT_EQ(re, 0);
-    }
-    re = metadata_client->removeLocalSegment("test_local_segment");
-    ASSERT_EQ(re, 0);
-}
-
-TEST_F(TransferMetadataTest, RpcMetaEntryTest) {
-    auto hostname_port = parseHostNameWithPort(local_server_name);
-    TransferMetadata::RpcMetaDesc desc;
-    desc.ip_or_host_name = hostname_port.first.c_str();
-    desc.rpc_port = hostname_port.second;
-    int re = metadata_client->addRpcMetaEntry("test_server", desc);
-    ASSERT_EQ(re, 0);
-    TransferMetadata::RpcMetaDesc desc1;
-    re = metadata_client->getRpcMetaEntry("test_server", desc1);
-    ASSERT_EQ(desc.ip_or_host_name, desc1.ip_or_host_name);
-    ASSERT_EQ(desc.rpc_port, desc1.rpc_port);
-    re = metadata_client->removeRpcMetaEntry("test_server");
-    ASSERT_EQ(re, 0);
-}
-
 namespace {
 
-struct ScopedMetadataRefreshConfig {
-    uint64_t old_interval_seconds;
-    bool old_metacache;
-
-    ScopedMetadataRefreshConfig(uint64_t interval_seconds, bool metacache)
-        : old_interval_seconds(
-              globalConfig().te_metadata_refresh_interval_seconds),
-          old_metacache(globalConfig().metacache) {
-        globalConfig().te_metadata_refresh_interval_seconds = interval_seconds;
-        globalConfig().metacache = metacache;
+class ScopedMetadataRefreshConfig {
+   public:
+    ScopedMetadataRefreshConfig(int interval, bool enabled) {
+        old_interval_ = TransferMetadata::getMetadataRefreshInterval();
+        old_enabled_ = TransferMetadata::getMetadataRefreshEnabled();
+        TransferMetadata::setMetadataRefreshInterval(interval);
+        TransferMetadata::setMetadataRefreshEnabled(enabled);
     }
 
     ~ScopedMetadataRefreshConfig() {
-        globalConfig().te_metadata_refresh_interval_seconds =
-            old_interval_seconds;
-        globalConfig().metacache = old_metacache;
+        TransferMetadata::setMetadataRefreshInterval(old_interval_);
+        TransferMetadata::setMetadataRefreshEnabled(old_enabled_);
     }
+
+   private:
+    int old_interval_;
+    bool old_enabled_;
 };
+
+constexpr TransferMetadata::SegmentID LOCAL_SEGMENT_ID = 0;
+
+uint16_t findAvailableTcpPort(int& sockfd) {
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) return 0;
+
+    int reuse = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    addr.sin_port = 0;
+    if (bind(sockfd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0) {
+        close(sockfd);
+        sockfd = -1;
+        return 0;
+    }
+    if (listen(sockfd, 16) < 0) {
+        close(sockfd);
+        sockfd = -1;
+        return 0;
+    }
+
+    socklen_t len = sizeof(addr);
+    if (getsockname(sockfd, reinterpret_cast<sockaddr*>(&addr), &len) < 0) {
+        close(sockfd);
+        sockfd = -1;
+        return 0;
+    }
+    return ntohs(addr.sin_port);
+}
 
 TransferMetadata::BufferDesc makeRdmaBufferDesc(uint64_t addr) {
     TransferMetadata::BufferDesc buffer_desc;
-    buffer_desc.name = "buffer";
+    buffer_desc.name = "rdma_buffer";
     buffer_desc.addr = addr;
-    buffer_desc.length = 1024;
-    buffer_desc.lkey.push_back(1);
-    buffer_desc.rkey.push_back(2);
+    buffer_desc.length = 4096;
     return buffer_desc;
 }
 
@@ -178,7 +102,6 @@ std::shared_ptr<TransferMetadata::SegmentDesc> makeMemoryPoolSegmentDesc(
     TransferMetadata::BufferDesc buffer_desc;
     buffer_desc.name = "memory_pool_buffer";
     buffer_desc.length = length;
-    buffer_desc.protocol = "memory_pool";
     buffer_desc.target_addr = target_addr;
     segment_desc->buffers.push_back(buffer_desc);
     return segment_desc;
@@ -279,7 +202,6 @@ TEST(TransferMetadataPollingTest, MemoryPoolTargetAddrRoundTrip) {
     ASSERT_TRUE(remote_desc);
     ASSERT_EQ(remote_desc->protocol, "memory_pool");
     ASSERT_EQ(remote_desc->buffers.size(), 1u);
-    ASSERT_EQ(remote_desc->buffers[0].protocol, "memory_pool");
     ASSERT_EQ(remote_desc->buffers[0].length, kLength);
     ASSERT_EQ(remote_desc->buffers[0].target_addr, kTargetAddr);
 
